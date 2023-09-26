d8d415eb6767e37d3670e399784cdea13221cf4c
Zero <john.royer@gmail.com>
2023-09-26 11:53:22 +0800

    create folder if not exists

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 # This is a sample Python script.
+import os.path
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-def create_file():
-    print('qq')
+def run():
+    print('run')
+
+    # put file in `repository` folder
+    prefix = 'repository/'
+    if not os.path.exists(prefix):
+        os.mkdir(prefix)
+
+def create_folder():
+    print('mkdir')
+
+def create_file(path, size):
+    print('create file')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print('creating files')
+    print('creating files ...')
+    run()
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
